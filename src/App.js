3ee4ad7061e344db6248aa7372c6486a939f4746
Estelle Choi <estele.choi@gmail.com>
2020-10-15 01:13:40 +0900

    Root component

--- src/App.js
+++ src/App.js
@@ -12,15 +12,45 @@ function App() {
 			subTitle:
 				"This is a shopping site of Bad Habit Club introducing brand archives and selling goods. Built with Vue.js.",
 			repoUrl: "https://github.com/estellechoi/badhabitclub-fe",
-			desc: [
+			img: [
 				{
-					title: "Preview",
-					content: {
-						src: "/portfolio/images/bhc1.png",
-						alt: "Bad Habit Club preview no.1",
-					},
+					src: "/portfolio/images/bhc1.png",
+					alt: "Bad Habit Club preview no.1",
+				},
+				{
+					src: "/portfolio/images/bhc2.png",
+					alt: "Bad Habit Club preview no.2",
+				},
+				{
+					src: "/portfolio/images/bhc3.png",
+					alt: "Bad Habit Club preview no.3",
+				},
+				{
+					src: "/portfolio/images/bhc4.png",
+					alt: "Bad Habit Club preview no.4",
+				},
+				{
+					src: "/portfolio/images/bhc5.png",
+					alt: "Bad Habit Club preview no.5",
+				},
+				{
+					src: "/portfolio/images/bhc6.png",
+					alt: "Bad Habit Club preview no.6",
+				},
+				{
+					src: "/portfolio/images/bhc7.png",
+					alt: "Bad Habit Club preview no.7",
+				},
+				{
+					src: "/portfolio/images/bhc8.png",
+					alt: "Bad Habit Club preview no.8",
+				},
+				{
+					src: "/portfolio/images/bhc9.png",
+					alt: "Bad Habit Club preview no.9",
 				},
 			],
+			desc: [],
 		},
 		{
 			idName: "canvas",
@@ -28,15 +58,13 @@ function App() {
 			subTitle:
 				"A simple painting board built with <canvas> element and pure JavaScript. Draw some lines, change line width and fill the whole canvas with your own color. Exporting your work on the canvas as a PNG file is also available.",
 			repoUrl: "https://github.com/estellechoi/canvas",
-			desc: [
+			img: [
 				{
-					title: "Preview",
-					content: {
-						src: "/portfolio/images/canvas.png",
-						alt: "Ruby Potato preview",
-					},
+					src: "/portfolio/images/canvas.png",
+					alt: "Ruby Potato preview",
 				},
 			],
+			desc: [],
 		},
 		{
 			idName: "cepo",
@@ -44,15 +72,13 @@ function App() {
 			subTitle:
 				"A draft markup for responsive website of CEPO, a company that develops GIS services.",
 			repoUrl: "https://github.com/estellechoi/cepo",
-			desc: [
+			img: [
 				{
-					title: "Preview",
-					content: {
-						src: "/portfolio/images/cepo.png",
-						alt: "CEPO homepage preview",
-					},
+					src: "/portfolio/images/cepo.png",
+					alt: "CEPO homepage preview",
 				},
 			],
+			desc: [],
 		},
 		{
 			idName: "ruby-potato",
@@ -60,12 +86,13 @@ function App() {
 			subTitle:
 				"Ruby Potato is a markdown editor for designers and front-end engineers to create their own design system.",
 			repoUrl: "https://github.com/estellechoi/ruby-potato",
-			desc: [
+			img: [
 				{
-					title: "Preview",
-					content: "",
+					src: "/portfolio/images/cepo.png",
+					alt: "CEPO homepage preview",
 				},
 			],
+			desc: [],
 		},
 		{
 			idName: "raycasting-3d-engine",
@@ -73,12 +100,13 @@ function App() {
 			subTitle:
 				"A Raycasting algorithm based engine that renders a semi-3D map in realtime, creating first-person view exploration of a maze-looking space with walls. Built with JavaScript.",
 			repoUrl: "https://github.com/estellechoi/javascript-raycasting",
-			desc: [
+			img: [
 				{
-					title: "Preview",
-					content: "",
+					src: "/portfolio/images/cepo.png",
+					alt: "CEPO homepage preview",
 				},
 			],
+			desc: [],
 		},
 	];
 
