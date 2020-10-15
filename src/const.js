2778dcf039b2e1d8b424a3e8ab25fdaa6d2965a0
Estelle Choi <estele.choi@gmail.com>
2020-10-16 03:43:14 +0900

    Static data were moved into const.js file

--- src/const.js
+++ src/const.js
@@ -1,78 +1,116 @@
-const prjts = [
+export const TITLE = "Front-end engineer Youjin's portfolio";
+
+export const prjts = [
 	{
 		idName: "bad-habit-club",
 		title: "Bad Habit Club",
 		subTitle:
 			"This is a shopping site of Bad Habit Club introducing brand archives and selling goods. Built with Vue.js.",
+		skills: ['html5', 'css3', 'js', 'vue'],
+		type: ['Website', 'Shopping mall'],
 		repoUrl: "https://github.com/estellechoi/badhabitclub-fe",
-		desc: [
+		img: [
 			{
-				title: "Preview",
-				content: {
-					src: "/portfolio/images/bhc1.png",
-					alt: "Bad Habit Club preview no.1",
-				},
+				src: "/portfolio/images/bhc1.png",
+				alt: "Bad Habit Club preview no.1",
+			},
+			{
+				src: "/portfolio/images/bhc2.png",
+				alt: "Bad Habit Club preview no.2",
+			},
+			{
+				src: "/portfolio/images/bhc3.png",
+				alt: "Bad Habit Club preview no.3",
+			},
+			{
+				src: "/portfolio/images/bhc4.png",
+				alt: "Bad Habit Club preview no.4",
+			},
+			{
+				src: "/portfolio/images/bhc5.png",
+				alt: "Bad Habit Club preview no.5",
+			},
+			{
+				src: "/portfolio/images/bhc6.png",
+				alt: "Bad Habit Club preview no.6",
+			},
+			{
+				src: "/portfolio/images/bhc7.png",
+				alt: "Bad Habit Club preview no.7",
+			},
+			{
+				src: "/portfolio/images/bhc8.png",
+				alt: "Bad Habit Club preview no.8",
+			},
+			{
+				src: "/portfolio/images/bhc9.png",
+				alt: "Bad Habit Club preview no.9",
 			},
 		],
+		desc: [],
 	},
 	{
 		idName: "canvas",
 		title: "Canvas",
 		subTitle:
 			"A simple painting board built with <canvas> element and pure JavaScript. Draw some lines, change line width and fill the whole canvas with your own color. Exporting your work on the canvas as a PNG file is also available.",
+		skills: ['html5', 'css3', 'js'],
+		type: ['Experiment'],
 		repoUrl: "https://github.com/estellechoi/canvas",
-		desc: [
+		img: [
 			{
-				title: "Preview",
-				content: {
-					src: "/portfolio/images/canvas.png",
-					alt: "Ruby Potato preview",
-				},
+				src: "/portfolio/images/canvas.png",
+				alt: "Ruby Potato preview",
 			},
 		],
+		desc: [],
 	},
 	{
 		idName: "cepo",
 		title: "CEPO homepage",
 		subTitle:
 			"A draft markup for responsive website of CEPO, a company that develops GIS services.",
+		skills: ['html5', 'css3'],
+		type: ['Demo'],
 		repoUrl: "https://github.com/estellechoi/cepo",
-		desc: [
+		img: [
 			{
-				title: "Preview",
-				content: {
-					src: "/portfolio/images/cepo.png",
-					alt: "CEPO homepage preview",
-				},
+				src: "/portfolio/images/cepo.png",
+				alt: "CEPO homepage preview",
 			},
 		],
+		desc: [],
 	},
 	{
 		idName: "ruby-potato",
 		title: "Ruby Potato",
 		subTitle:
 			"Ruby Potato is a markdown editor for designers and front-end engineers to create their own design system.",
+		skills: ['html5', 'css3', 'js', 'react', 'styled-components'],
+		type: ['Experiment'],
 		repoUrl: "https://github.com/estellechoi/ruby-potato",
-		desc: [
+		img: [
 			{
-				title: "Preview",
-				content: "",
+				src: "/portfolio/images/cepo.png",
+				alt: "CEPO homepage preview",
 			},
 		],
+		desc: [],
 	},
 	{
 		idName: "raycasting-3d-engine",
 		title: "Raycasting 3D Engine",
 		subTitle:
 			"A Raycasting algorithm based engine that renders a semi-3D map in realtime, creating first-person view exploration of a maze-looking space with walls. Built with JavaScript.",
+		skills: ['js'],
+		type: ['Experiment'],
 		repoUrl: "https://github.com/estellechoi/javascript-raycasting",
-		desc: [
+		img: [
 			{
-				title: "Preview",
-				content: "",
+				src: "/portfolio/images/cepo.png",
+				alt: "CEPO homepage preview",
 			},
 		],
+		desc: [],
 	},
 ];
-
-export default { prjts };
