cbfc6cbcd7e708fd50413fdbcd5b4d396efcaee2
Estelle Choi <estele.choi@gmail.com>
2020-10-17 23:12:37 +0900

    A callback for child component was expressed and passed as a prop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import "./App.css";
 import { TITLE, prjts } from "./const"
@@ -10,12 +10,21 @@ const APP = styled.div`
 	position: relative;
 `
 
+
+
 export default function App() {
+	const [activeId, setActiveId] = useState(null);
+
+	const observerHandler = (entry) => {
+		console.log(entry);
+		setActiveId(entry.target.id);
+	}
+
 	return (
 		<APP>
 			<HiddenBox html={<h1>{TITLE}</h1>}></HiddenBox>
-			<Header prjts={prjts}></Header>
-			<Main prjts={prjts}></Main>
+			<Header prjts={prjts} activeId={activeId}></Header>
+			<Main prjts={prjts} observerHandler={observerHandler}></Main>
 		</APP>
 	);
 }
